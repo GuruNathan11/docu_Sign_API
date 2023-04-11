e961ffa0a4e169f24d82cc506d6c6ea59cfff21d
GuruNathan11 <121783817+GuruNathan11@users.noreply.github.com>
2023-04-11 17:53:09 +0530

    Add files via upload

---
+++ controllers/envelopesController.js
@@ -0,0 +1,11 @@
+const envelopesService = require('../services/envelopesService.js');
+
+exports.createEnvelope = async (req, res) => {
+  try {
+    const { documentUrl, email, name } = req.body;
+    const envelope = await envelopesService.createEnvelope(documentUrl, email, name);
+    res.status(201).json(envelope);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+// app.use(bodyParser.json());
+app.use(cors());
+
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+// Importing the routes
+const envelopesRoutes = require('./routes/envelopes');
+
+// app.use(bodyParser.json());
+app.use('/envelopes', envelopesRoutes);
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req,res) => res.send ("Welcome to Docu sign API"));
